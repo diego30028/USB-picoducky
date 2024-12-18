e535bbd9525d6f6d7f6a32745ece6d5cfe808549
diego30028 <85185886+diego30028@users.noreply.github.com>
2024-12-18 17:23:43 -0500

    Add files via upload

---
+++ code_us.py
@@ -0,0 +1,41 @@
+import time
+import board
+import digitalio
+import usb_hid
+from adafruit_hid.keycode import Keycode
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.consumer_control import ConsumerControl
+from adafruit_hid.consumer_control_code import ConsumerControlCode
+time.sleep(2.5)
+# Set up Consumer Control - Control Codes can be found here: https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/consumer_control_code.html#ConsumerControlCode
+cc = ConsumerControl(usb_hid.devices)
+
+# Set up a keyboard device. - Keycode can be found here: https://docs.circuitpython.org/projects/hid/en/latest/_modules/adafruit_hid/keycode.html#Keycode
+keyboard = Keyboard(usb_hid.devices)
+
+# Set up keyboard to write strings from macro
+write_text = KeyboardLayoutUS(keyboard)
+
+
+
+btn1_pin = board.GP0
+btn1 = digitalio.DigitalInOut(btn1_pin)
+btn1.direction = digitalio.Direction.INPUT
+btn1.pull = digitalio.Pull.DOWN
+def payload():
+    time.sleep(0.1)
+    #enter your payload here
+
+
+
+    
+    
+    # Keycode class defines USB HID keycodes to send using Keyboard.  
+if btn1.value:
+   print("edit mode")
+else:
+    payload()
+        
+    
+time.sleep(0.1)
